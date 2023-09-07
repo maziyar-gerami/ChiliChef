5cda4fdd933795b6623dd9f74a87115063a36455
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-08 00:00:00 +0330

    ItemClickListener.java modified

---
+++ src/main/java/com/ict/chilichef/Adapter/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.ict.chilichef.Adapter;
+
+import android.view.View;
+
+/**
+ * Created by Elham on 8/16/2017.
+ */
+
+public interface ItemClickListener {
+    void onItemClick(View v, int layoutPosition);
+}
