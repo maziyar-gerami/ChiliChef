1c0fceea2c872440ae1ebcb6acb259bb6c1afc38
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-08 00:00:00 +0330

    ViewPagerAdapter.java modified

---
+++ src/main/java/com/ict/chilichef/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.ict.chilichef.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Elham on 4/7/2017.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> tabTitles = new ArrayList<>();
+
+    public void addFragments(Fragment fragments, String titles) {
+
+        this.fragments.add(fragments);
+        this.tabTitles.add(titles);
+    }
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles.get(position);
+    }
+
+
+    @Override
+    public int getItemPosition(Object item) {
+        return POSITION_NONE;
+    }
+
+
+}
