a150006e6bf5dea8cb19f2106089341dd97a49c7
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-08 00:00:00 +0330

    TabFoodActivity.java modified

---
+++ src/main/java/com/ict/chilichef/NavigationItems/Categories/TabFoodActivity.java
@@ -0,0 +1,122 @@
+package com.ict.chilichef.NavigationItems.Categories;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.ict.chilichef.Adapter.ViewPagerAdapter;
+import com.ict.chilichef.R;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+
+public class TabFoodActivity extends AppCompatActivity {
+
+    private ArrayList<Bundle> arrayListBundle = new ArrayList<>();
+    private TabLayout.Tab tab;
+    private ImageButton back;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_food);
+
+        Bundle bundle = getIntent().getExtras();
+
+        int cat = bundle.getInt("ItemID");
+        String TITLE = bundle.getString("ItemName");
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.Toolbar_Public);
+        setSupportActionBar(toolbar);
+        ((TextView) findViewById(R.id.PublicTitle_toolbar)).setText(TITLE);
+
+        back = (ImageButton) findViewById(R.id.PublicLeft_icon_id);
+        back.setImageResource(R.drawable.ic_back);
+        back.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TabFoodActivity.this.finish();
+            }
+        });
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.Categories_viewPager);
+        setupViewPager(viewPager, cat);
+        int x = setupViewPager(viewPager, cat);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.Categories_tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tab = tabLayout.getTabAt(x - 1);
+
+        if (tab != null) {
+
+            tab.select();
+
+        }
+
+    }
+
+
+    private int setupViewPager(ViewPager viewPager, int cat) {
+
+        ViewPagerAdapter Adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        SQLiteDatabase myDatabase = getApplicationContext().openOrCreateDatabase(getString(R.string.databasePath), MODE_PRIVATE, null);
+
+        Cursor cursor = myDatabase.rawQuery("SELECT * FROM categories WHERE Parent=+" + cat, null);
+
+        cursor.moveToFirst();
+
+        int counter = 0;     // a counter for counting tabs
+
+        List<String> titles = new LinkedList<>();
+
+        while (!cursor.isAfterLast()) {
+
+            int id_tab = cursor.getInt(cursor.getColumnIndex("Id_Cat"));
+            arrayListBundle.add(new Bundle());
+            arrayListBundle.get(counter).putInt("Id_Cat", id_tab);
+            titles.add(cursor.getString(cursor.getColumnIndex("Name")));
+
+            cursor.moveToNext();
+            counter++;
+        }
+
+        int i;
+
+        for (i = counter - 1; i >= 0; i--) {
+
+            CategoriesFragment categoriesFragment = new CategoriesFragment();
+            categoriesFragment.setArguments(arrayListBundle.get(i));
+            Adapter.addFragments(categoriesFragment, titles.get(i));
+        }
+
+        cursor.close();
+
+        viewPager.setAdapter(Adapter);
+        return counter;
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+
+    }
+
+}
