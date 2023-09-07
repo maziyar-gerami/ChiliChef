f9c8d36b82ef5f6cb77354b020805d9c54e29b27
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-08 00:00:00 +0330

    DataUserGallery.java modified

---
+++ src/main/java/com/ict/chilichef/Model/DataUserGallery.java
@@ -0,0 +1,22 @@
+package com.ict.chilichef.Model;
+
+/**
+ * Created by roshan on 4/14/2017.
+ */
+
+public class DataUserGallery {
+
+
+    public int imageUserId;
+
+    public DataUserGallery(int imageUserId) {
+
+        this.imageUserId = imageUserId;
+    }
+
+
+
+    public int getImageUserId() {
+        return imageUserId;
+    }
+}
