4cddc8fa714628c883c111fda78eb888430f8317
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-08 00:00:00 +0330

    UsersStatusModel.java modified

---
+++ src/main/java/com/ict/chilichef/Model/UsersStatusModel.java
@@ -0,0 +1,18 @@
+package com.ict.chilichef.Model;
+
+/**
+ * Created by NP on 10/29/2017.
+ */
+
+public class UsersStatusModel {
+
+    private int Status;
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+}
